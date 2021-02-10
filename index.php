8fa6ca9fffc5234216630a791c33d328c35ef338
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-02-10 15:02:16 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        
+        <header>
+            <h1>Jean Forteroche</h1>
+            <nav>
+                <!-- retour page principale -->
+                <!-- répertoire -->
+                <!-- connection -->
+            </nav>
+        </header>
+        <section>
+            <div class="article"> <!-- dernier article --> 
+                <div class="label"> <!-- encart label -->
+                    <h2 class="titreArtile"></h2>
+                    <div class="articleDate"> </div>
+                </div>
+                <div class="preview">
+                    <!-- premières lignes de l'article -->
+                    <a class="lireLaSuite" href="#">Lire la suite</a>
+                </div>
+            </div>
+            <div class="article"></div>
+            <div class="article"></div>
+            <div class="article"></div>
+            <div class="article"></div>
+        </section>
+        <footer></footer>
+    </body>
+</html>
