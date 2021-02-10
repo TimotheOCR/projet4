396db230b7fae89af672cccfa9469efa507aa919
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-02-10 15:03:48 +0100

    Create database.php

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+    class Database {
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=jeanforteroche;charset=utf8', 'root', '');
+        }
+        catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+?>
