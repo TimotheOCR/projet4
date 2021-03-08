afe97539b869a452d2efa23427397848435f5423
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-03-08 10:13:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
-    require 'database.php';
-    require 'article.php';
+    require 'class/database.php';
+    require 'class/article.php';
+    require 'manager/articleManager.php';
+    require 'class/utilisateur.php';
+    require 'manager/utilisateurManager.php';
+    
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8"/>
     </head>
-    <body>   
+    <body>
+        
         <header>
             <h1>Jean Forteroche</h1>
             <nav>
@@ -17,28 +23,51 @@
             </nav>
         </header>
         <section>
-             <?php 
-                $article = new Article();
-                $reponse = $article->getArticle();
+            <?php 
+                $db= new Database();
+                $connection = $db->getConnection(); 
+
+
+
+                $jean = new Utilisateur([
+                    'id' => '',
+                    'pseudo' => 'jean',
+                    'email' => 'jean@mail.com',
+                    'password' => '123',
+                    'commentaires' => ''
+                ]);
+
+                $pushJean = new UtilisateurManager($connection);
+                $pushJean->add($jean);
+
+                echo $pushJean->getUtilisateur(['pseudo']);
+
+
+                
+                $article = new ArticleManager($connection);
+                $reponse = $article->getArticles();
                 while ($article = $reponse->fetch()) 
                 {
             ?>
-            <div class="article">  <?php echo $article['titre']; ?> <!-- dernier article --> 
+                
+            <div class="article"> <?php echo $article['titre']; ?><!-- dernier article --> 
                 <div class="label"> <!-- encart label -->
                     <h2 class="titreArtile"></h2>
                     <div class="articleDate"> </div>
                 </div>
-             <?php
-              }
+        
+                <div class="preview"><?php echo $article['contenu']; ?>
+                
+                <!-- premières lignes de l'article -->
+                <a class="lireLaSuite" href="#">Lire la suite</a>
+                </div>
+            </div>
+            <?php
+                }
 
                 $reponse->closeCursor(); // Termine le traitement de la requête
 
             ?>
-                <div class="preview">
-                    <!-- premières lignes de l'article -->
-                    <a class="lireLaSuite" href="#">Lire la suite</a>
-                </div>
-            </div>
             <div class="article"></div>
             <div class="article"></div>
             <div class="article"></div>
