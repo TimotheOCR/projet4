989080cdb76a86fd98b4efc3ce81465157092624
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-03-10 20:31:32 +0100

    Update utilisateurManager.php

--- manager/utilisateurManager.php
+++ manager/utilisateurManager.php
@@ -4,21 +4,27 @@ class UtilisateurManager {
 
     private $_db;
 
+   
     public function __construct(){
         $bdd = new Database;
         $this->_db = $bdd->getConnection();
     }
 
     public function add (Utilisateur $utilisateur){
-        $q = $this->_db->prepare('INSERT INTO utilisateur(ID, pseudo, email, Password, commentaires)');
+        $bdd = new Database;
+        $co = $bdd->getConnection();
+        
+        $q = $this->$co->prepare('INSERT INTO utilisateur(ID, pseudo, email, password, commentaires) VALUE(:ID, :pseudo, :email, :password, :commentaires)');
         $q->bindValue(':ID', $utilisateur->ID(), PDO::PARAM_INT);
         $q->bindValue(':pseudo', $utilisateur->pseudo(), PDO::PARAM_STR);
         $q->bindValue(':email', $utilisateur->email(), PDO::PARAM_STR);
         $q->bindValue(':password', $utilisateur->password(), PDO::PARAM_STR);
         $q->bindValue(':commentaires', $utilisateur->commentaires(), PDO::PARAM_STR);
+        
 
         $q->execute();
     }
+
 
     public function delete(Utilisateur $utilisateur){
         $this->_db->exec('DELETE FROM utilisateur WHERE ID = ' .$utilisateur->ID());
