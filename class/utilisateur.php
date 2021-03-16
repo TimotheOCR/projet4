8038fdd4a0c04eea54ceea40a1be2cd8e4585dd4
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-03-16 16:10:04 +0100

    Update utilisateur.php

--- class/utilisateur.php
+++ class/utilisateur.php
@@ -1,55 +1,51 @@
 <?php 
-    class Utilisateur {
+    class Utilisateur extends Hydrate{
         private $_id;
         private $_pseudo;
         private $_email;
         private $_password;
         private $_commentaires;
-        
-        public function hydratation (){
-            Hydrate::__construct();
-        }
 
         public function id() {return $this->_id;}
         public function pseudo() {return $this->_pseudo;}
         public function email() {return $this->_email;}
-        public function password() {return $this->_password;} // private ?
+        public function password() {return $this->_password;}
         public function commentaires() {return $this->_commentaires;}
 
 
         public function getId(){
             return $this->id;
         }
-        public function setId(){
-            $this->id = $id;
+        public function setId($id){
+            $this->_id = $id;
         }
 
         public function getPseudo(){
             return $this->pseudo;
         }
-        public function setPseudo(){
-            $this->pseudo = $pseudo;
+        public function setPseudo($pseudo){
+            $this->_pseudo = $pseudo;
         }
 
         public function getEmail(){
             return $this->email;
         }
-        public function setEmail(){
-            $this->email = $email;
+        public function setEmail($email){
+            $this->_email = $email;
         }
 
         public function getPassword(){
             return $this->password;
         }
-        public function setPassword(){
-            $this->password = $password;
+        public function setPassword($password){
+            $this->_password = $password;
         }
 
         public function getCommentaires(){
             return $this->commentaires;
         }
-        public function setCommentaires(){
-            $this->commentaires = $commentaires;
+        public function setCommentaires($commentaires){
+            $this->_commentaires = $commentaires;
         }    
     }
 
