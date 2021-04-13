6b93cd7072f072e1cee2fbe62a6dca38b4fb6db2
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-04-13 08:36:12 +0200

    Update ArticleManager.php

--- models/ArticleManager.php
+++ models/ArticleManager.php
@@ -14,7 +14,7 @@ class ArticleManager extends Model{
         $q->execute();
     }
     public function updateArticle($id){
-        return $this->uptade('articles', 'Article', $id);
+        return $this->uptade('articles', '\Models\Article', $id);
     }
 
     public function deleteArticle($id){
@@ -22,11 +22,11 @@ class ArticleManager extends Model{
     }
 
     public function getArticles (){
-        return $this->getAll('articles', 'Article');
+        return $this->getAll('articles', '\Models\Article');
     }
 
     public function getArticle($id){
-        return $this->getOne('articles', 'Article', $id);
+        return $this->getOne('articles', '\Models\Article', $id);
     }
 }
 ?>
