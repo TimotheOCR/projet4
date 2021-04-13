ccb38aac33b058e14534328279fd264f464c8fcc
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-04-13 07:49:17 +0200

    Update Router.php

--- controllers/Router.php
+++ controllers/Router.php
@@ -1,40 +1,42 @@
 <?php
+    namespace Controllers;
+
+    use Controllers\ControllerAccueil;
+    
     class Router {
         private $_ctrl;
         private $_view;
 
         public function routReq(){
-            try { //chargement des classes
-                spl_autoload_register(function($class){
-                    require_once('models/'.$class.'.php');
-                });
+           
 
-                $url = '';
+            $url = '';
 
-                if(isset($_GET['url'])){
-                    $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL));
+            if(isset($_GET['url'])){
+                $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL));
 
-                    $controller = ucfirst(strtolower($url[0]));
-                    $controllerClass = "Controller".$controller;
-                    $controllerFile = "controllers/".$controllerClass.".php";
+                $controller = ucfirst(strtolower($url[0]));
+                $controllerClass = "Controller".$controller;
+                $controllerFile = "controllers/".$controllerClass.".php";
 
-                    if(file_exists($controllerFile)){
-                        require_once($controllerFile);
-                        $this->_ctrl = new $controllerClass($url);
-                    }else{
-                        throw new Exception('Page introuvable');
-                    }
-                }
-                else 
-                {
-                    require_once('controllers/ControllerAccueil.php');
-                    $this->_ctrl = new ControllerAccueil($url);
+                if(file_exists($controllerFile)){
+                    require_once($controllerFile);
+                    $this->_ctrl = new $controllerClass($url);
+                }else{
+                    throw new Exception('Page introuvable');
                 }
             }
-            catch(Exception $e) {
-                $errorMsg = $e->getMessage();
-                require_once('views/viewError.php');
+            else 
+            {
+                // require_once('controllers/ControllerAccueil.php');
+                $this->_ctrl = new ControllerAccueil($url);
+                // ControllerAccueil::__construct();
             }
+            
+            // catch(Exception $e) {
+            //     $errorMsg = $e->getMessage();
+            //     require_once('views/viewError.php');
+            // }
         }
     }
 ?>
