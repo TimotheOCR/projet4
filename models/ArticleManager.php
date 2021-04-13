f2bf4283f2a77e863fedaf2895226aae184f0af9
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-04-13 07:40:41 +0200

    Update ArticleManager.php

--- models/ArticleManager.php
+++ models/ArticleManager.php
@@ -1,20 +1,32 @@
 <?php
+namespace Models;
+use Models\Model;
+use Models\Article;
 
 class ArticleManager extends Model{
 
+    public function add (Article $article){
+        $q = $this->getBdd()->prepare('INSERT INTO articles(ID, titre, contenu, auteur_id, date) VALUE(0, :titre, :contenu, :auteur_id, :date)');
+        $q->bindValue(':titre', $article->titre(), PDO::PARAM_STR);
+        $q->bindValue(':contenu', $article->contenu(), PDO::PARAM_STR);
+        $q->bindValue(':auteur_id', $article->auteur(), PDO::PARAM_STR);
+        $q->bindValue(':date', $article->date(), PDO::PARAM_STR);
+        $q->execute();
+    }
+    public function updateArticle($id){
+        return $this->uptade('articles', 'Article', $id);
+    }
 
-    public function delete(Article $article){
-        $this->exec('DELETE FROM articles WHERE ID = ' .$article->ID());
+    public function deleteArticle($id){
+        return $this->delete('articles', $id);
     }
 
     public function getArticles (){
         return $this->getAll('articles', 'Article');
     }
 
-    public function getId($id){
-        $result = query('SELECT ID FROM articles');
-        return $result;
+    public function getArticle($id){
+        return $this->getOne('articles', 'Article', $id);
     }
-
 }
 ?>
