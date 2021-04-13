3782f450463a2f2a124db39f9a261cd982c40d54
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-04-13 07:44:13 +0200

    Update Hydrate.php

--- models/Hydrate.php
+++ models/Hydrate.php
@@ -1,16 +1,16 @@
 <?php
-
+namespace Models;
 class Hydrate {
 
-    public function __construct (array $donnees = array()){
-        if (!empty($donnees)){
-            $this->hydrate($donnees);
+    public function __construct (array $data){
+        if (!empty($data)){
+            $this->hydrate($data);
         }
         
     }
     
-    public function hydrate (array $donnees){
-        foreach ($donnees as $key => $value){
+    public function hydrate (array $data){
+        foreach ($data as $key => $value){
             $method = 'set'.ucfirst($key);
             if (method_exists($this, $method))
             {
@@ -19,6 +19,5 @@ class Hydrate {
         }
     }
 }
-
 
 ?>
