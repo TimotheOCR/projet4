4e3003881044759e3a994d95186d7f0516207dbf
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-04-13 07:46:45 +0200

    Update UtilisateurManager.php

--- models/UtilisateurManager.php
+++ models/UtilisateurManager.php
@@ -1,43 +1,28 @@
 <?php
+namespace Models;
+use Models\Model;
+use Models\Utilisateur;
 
-class UtilisateurManager  {
-
-    private $_db;
-    
-
-    public function __construct(){
-        $bdd = new Database;
-        $this->_db = $bdd->getConnection();
-    }
+class UtilisateurManager extends Model {
 
     public function add (Utilisateur $utilisateur){
-
         $q = $this->_db->prepare('INSERT INTO utilisateur(ID, pseudo, email, password, commentaires) VALUE(0, :pseudo, :email, :password, :commentaires)');
         $q->bindValue(':pseudo', $utilisateur->pseudo(), PDO::PARAM_STR);
         $q->bindValue(':email', $utilisateur->email(), PDO::PARAM_STR);
         $q->bindValue(':password', $utilisateur->password(), PDO::PARAM_STR);
         $q->bindValue(':commentaires', $utilisateur->commentaires(), PDO::PARAM_STR);
-        
-
         $q->execute();
     }
-  
-    public function delete(Utilisateur $utilisateur){
-        $this->_db->exec('DELETE FROM utilisateur WHERE ID = ' .$utilisateur->ID());
-    }
 
-    public function getUtilisateur (){
-        $db= new Database();
-        $connection = $db->getConnection(); //methode Database
-        $result = $connection->query('SELECT * FROM utilisateur');
-        return $result;
+    public function deleteUtilisateur ($id){
+        echo ' je suis deleteUtilisateur() et l\'id ';
+        return $this->delete('utilisateur', $id);
     }
-
-    public function getId(){
-        $db = new Database();
-        $connection = $db->getConnection();
-        $result = $connection->query('SELECT ID FROM utilisateur');
-        return $result;
+    public function getUtilisateurs (){
+        return $this->getAll('utilisateurs', 'Utilisateur');
+    }
+    public function getUtilisateur ($id){     
+        return $this->getOne('utilisateur', 'Utilisateur', $id);
     }
 
 }
