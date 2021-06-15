99e92cbf45b433a063091ee41eb065061e60c2cb
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-06-15 15:24:41 +0200

    Update ControllerArticle.php

--- controllers/ControllerArticle.php
+++ controllers/ControllerArticle.php
@@ -4,60 +4,48 @@
         use Views\View;
         use Models\ArticleManager;
         use Models\Article;
-    //  use Models\CommentaireManager;
     require_once ('views/View.php');
-
         echo 'ControllerArticle';
 
 
         class ControllerArticle {
         private $_article;
-        // private $_commentaireManager;
-
+       
         public function __construct($url){
-            if (isset($url) && count($url) > 1){
-                if ($url[1] == "post" && isset($_POST)) {
-                    if($_POST['update']){
-                        $this->update($_POST['id']);
-                    }else{
-                        $this->new();
-                    }
-                }
-                if($url[2] == "update"){
-                    $id = $url[1];
-                    
-                    $this->_article = new ArticleManager();
-                    $article = $this->_article->getArticle($id);
-                    $this->_view = new View('Publicate');              
-                    $this->_view->generate(array('article' => $article, 'update' => 'true'));
+           $this->init($url);
+        }
 
-                }
-                if($url[1] == "delete"){
-                    $this->delete($url[2]);
-                }
-                else {
-                    $this->article($url[1]);              
-                }
+        private function init($url){
+            $methode = $url[1];
+            if(isset($url[2]) && is_numeric($url[2])){
+                $this->$methode($url[2]);
             }
             else{
-                $this->getAll();
+                $this->$methode();     
             }
         }
         private function update($id){
             $this->_article = new ArticleManager();
-            $article = $this->_article->updateArticle($id);
+            $article = $this->_article->getArticle($id);
+            $this->_view = new View('Publicate');              
+            $this->_view->generate(array('article' => $article, 'update' => 'true'));
+            echo('methode update');
+            echo ($id);
 
         }
-        private function new(){           
+        private function post(){   
+            $article = new Article($_POST);  
+            $manager = new ArticleManager();        
             if(!empty($_POST)){
-                if(isset($_POST['titre'], $_POST['contenu'], $_POST['auteur']) && !empty($_POST['titre'] && !empty($_POST['contenu'] && !empty($_POST['auteur'])))) {
-                      
-                    $article = new Article($_POST);  
-                    $controller = new ArticleManager();
-                    $controller->add($article);
-                    echo("L'article est publié");
+                if($_POST['update']){
+                    $manager->updateArticle($article);
                 }else{
-                die("Le formulaire est incomplet");
+                    if(isset($_POST['titre'], $_POST['contenu'], $_POST['auteur']) && !empty($_POST['titre'] && !empty($_POST['contenu'] && !empty($_POST['auteur'])))) {
+                        $manager->add($article);
+                        echo("L'article est publié");
+                    }else{
+                    die("Le formulaire est incomplet");
+                    }
                 }
             }
             $this->getAll();
@@ -66,7 +54,7 @@
             $this->_article = new ArticleManager();
             $article = $this->_article->deleteArticle($id);
         }
-        private function article($id){
+        private function getOne($id){
 
             $this->_article = new ArticleManager();
             $article = $this->_article->getArticle($id);       
@@ -81,13 +69,7 @@
             $this->_view = new View('Accueil');
             $this->_view->generate(array('articles' => $articles));
         }         
-        // private function commentaires(){
-        //     $this->_commentaireManager = new CommentaireManager();
-        //     $commentaires = $this->_commentaireManager->getCommentaires();
 
-        //     $this->_view = new View('Article');
-        //     $this->_view->generate(array('commentaires' => $commentaires));
-        // }
     }
      
 
