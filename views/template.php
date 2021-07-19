3f2835ad453c0aa23b0e8f6eee3b3d19621d998b
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-07-19 09:47:17 +0200

    Update template.php

--- views/template.php
+++ views/template.php
@@ -2,12 +2,20 @@
 <!DOCTYPE html>
 <html>
     <head>
+         <meta charset="utf-8" />
+        <meta name="description" content="Billet simple pour l'Alaska, blog de Jean Forteroche"/>
+        <meta property="og:title" content="Billet simple pour l'Alaska"/>
+        <meta property="og:image" content="images/Alaska.png"/> 
+        <meta property="og:url" content="http://www.timothe-tornatore.fr/projet4/"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Jean Forteroche"/>
+        <meta property="og:description, fb:app_id"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
+        <link rel="icon" href="images/plume.png"/>
         <script src="https://cdn.tiny.cloud/1/8543kzkkzt3pplrx9dli68ptqdzj3dslceufo2gdejox2i96/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
         <script src="https://kit.fontawesome.com/4463c9b046.js" crossorigin="anonymous"></script>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?= $t ?></title>
+        <title>Jean Forteroche</title>
     </head>
     <body>
         <div id="page-container">
