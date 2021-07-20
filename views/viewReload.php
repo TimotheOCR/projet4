7cef0d33fd7e4b96df06a3839f01a62e6aa186b5
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-07-20 10:29:48 +0200

    Update viewReload.php

--- views/viewReload.php
+++ views/viewReload.php
@@ -1,12 +1,5 @@
 <?php
 session_start();
-// function re (){
-//     ob_start();
-//     header('Location: ' . $_SERVER['HTTP_REFERER']);
-//     echo('bruh');
-//     ob_end_flush();
-// }
-
 ?>
 <section id="reload">
     <div class="reload">
