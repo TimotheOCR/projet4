9970446118144e8144dbfee942b110965d57cf64
TimotheOCR <78786890+TimotheOCR@users.noreply.github.com>
2021-07-21 14:04:06 +0200

    Update viewReload.php
    affichage erreur log in

--- views/viewReload.php
+++ views/viewReload.php
@@ -1,9 +1,18 @@
 <?php
 session_start();
+
 ?>
+
 <section id="reload">
+<?php
+if(isset($wrong) && $wrong == true): ?>
+    <div class="reload">
+        <p> Utilisateur inconnu</p>
+    </div>
+<?php else: ?>
     <div class="reload">
         <p> votre signalement a été pris en compte merci </p>
         <button onclick="history.back()">retour</button>
     </div>
+<?php endif ?>
 </section>
